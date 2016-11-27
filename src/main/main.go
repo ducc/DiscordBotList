79793aa863d4ea057a38cf82dd2eee13635201b1
SpongyBacon <spongybacon@hotmail.co.uk>
2016-11-27 10:29:16 +0000

    models

--- src/main/main.go
+++ src/main/main.go
@@ -29,10 +29,49 @@ func main() {
         Directory: conf.templates.directory,
     }, "base:templates"))
     m.Get("/", func(ctx *macaron.Context) {
+        ctx.Data["featured_bots"] = []botModel{
+            {id: "123456789", name: "Vonodosh Bot", category: "Games", description: "Vonodosh. A bot that can fulfill your entertaining needs, with games to keep your guild busy.", website: "http://bit.ly/vonodosh", server: "PVHQxBd", authorize: "https://discordapp.com/oauth2/authorize?client_id=192332191240421377&scope=bot&permissions=536083519", avatar: "https://cdn.discordapp.com/app-icons/170242612425392128/8744452b039eaec1ba34c0a96c3bf428.jpg", online: true, ranking: 53, servers: 484, featured: false, score: 4, reviews: 835, },
+            {id: "123456789", name: "Vonodosh Bot", category: "Games", description: "Vonodosh. A bot that can fulfill your entertaining needs, with games to keep your guild busy.", website: "http://bit.ly/vonodosh", server: "PVHQxBd", authorize: "https://discordapp.com/oauth2/authorize?client_id=192332191240421377&scope=bot&permissions=536083519", avatar: "https://cdn.discordapp.com/app-icons/170242612425392128/8744452b039eaec1ba34c0a96c3bf428.jpg", online: true, ranking: 53, servers: 484, featured: false, score: 4, reviews: 835, },
+            {id: "123456789", name: "Vonodosh Bot", category: "Games", description: "Vonodosh. A bot that can fulfill your entertaining needs, with games to keep your guild busy.", website: "http://bit.ly/vonodosh", server: "PVHQxBd", authorize: "https://discordapp.com/oauth2/authorize?client_id=192332191240421377&scope=bot&permissions=536083519", avatar: "https://cdn.discordapp.com/app-icons/170242612425392128/8744452b039eaec1ba34c0a96c3bf428.jpg", online: true, ranking: 53, servers: 484, featured: false, score: 4, reviews: 835, },
+            {id: "123456789", name: "Vonodosh Bot", category: "Games", description: "Vonodosh. A bot that can fulfill your entertaining needs, with games to keep your guild busy.", website: "http://bit.ly/vonodosh", server: "PVHQxBd", authorize: "https://discordapp.com/oauth2/authorize?client_id=192332191240421377&scope=bot&permissions=536083519", avatar: "https://cdn.discordapp.com/app-icons/170242612425392128/8744452b039eaec1ba34c0a96c3bf428.jpg", online: true, ranking: 53, servers: 484, featured: false, score: 4, reviews: 835, },
+            {id: "123456789", name: "Vonodosh Bot", category: "Games", description: "Vonodosh. A bot that can fulfill your entertaining needs, with games to keep your guild busy.", website: "http://bit.ly/vonodosh", server: "PVHQxBd", authorize: "https://discordapp.com/oauth2/authorize?client_id=192332191240421377&scope=bot&permissions=536083519", avatar: "https://cdn.discordapp.com/app-icons/170242612425392128/8744452b039eaec1ba34c0a96c3bf428.jpg", online: true, ranking: 53, servers: 484, featured: false, score: 4, reviews: 835, },
+        }
+        ctx.Data["top_rated_bots"] = []botModel{
+            {id: "123456789", name: "Vonodosh Bot", category: "Games", description: "Vonodosh. A bot that can fulfill your entertaining needs, with games to keep your guild busy.", website: "http://bit.ly/vonodosh", server: "PVHQxBd", authorize: "https://discordapp.com/oauth2/authorize?client_id=192332191240421377&scope=bot&permissions=536083519", avatar: "https://cdn.discordapp.com/app-icons/170242612425392128/8744452b039eaec1ba34c0a96c3bf428.jpg", online: true, ranking: 53, servers: 484, featured: false, score: 4, reviews: 835, },
+            {id: "123456789", name: "Vonodosh Bot", category: "Games", description: "Vonodosh. A bot that can fulfill your entertaining needs, with games to keep your guild busy.", website: "http://bit.ly/vonodosh", server: "PVHQxBd", authorize: "https://discordapp.com/oauth2/authorize?client_id=192332191240421377&scope=bot&permissions=536083519", avatar: "https://cdn.discordapp.com/app-icons/170242612425392128/8744452b039eaec1ba34c0a96c3bf428.jpg", online: true, ranking: 53, servers: 484, featured: false, score: 4, reviews: 835, },
+            {id: "123456789", name: "Vonodosh Bot", category: "Games", description: "Vonodosh. A bot that can fulfill your entertaining needs, with games to keep your guild busy.", website: "http://bit.ly/vonodosh", server: "PVHQxBd", authorize: "https://discordapp.com/oauth2/authorize?client_id=192332191240421377&scope=bot&permissions=536083519", avatar: "https://cdn.discordapp.com/app-icons/170242612425392128/8744452b039eaec1ba34c0a96c3bf428.jpg", online: true, ranking: 53, servers: 484, featured: false, score: 4, reviews: 835, },
+            {id: "123456789", name: "Vonodosh Bot", category: "Games", description: "Vonodosh. A bot that can fulfill your entertaining needs, with games to keep your guild busy.", website: "http://bit.ly/vonodosh", server: "PVHQxBd", authorize: "https://discordapp.com/oauth2/authorize?client_id=192332191240421377&scope=bot&permissions=536083519", avatar: "https://cdn.discordapp.com/app-icons/170242612425392128/8744452b039eaec1ba34c0a96c3bf428.jpg", online: true, ranking: 53, servers: 484, featured: false, score: 4, reviews: 835, },
+            {id: "123456789", name: "Vonodosh Bot", category: "Games", description: "Vonodosh. A bot that can fulfill your entertaining needs, with games to keep your guild busy.", website: "http://bit.ly/vonodosh", server: "PVHQxBd", authorize: "https://discordapp.com/oauth2/authorize?client_id=192332191240421377&scope=bot&permissions=536083519", avatar: "https://cdn.discordapp.com/app-icons/170242612425392128/8744452b039eaec1ba34c0a96c3bf428.jpg", online: true, ranking: 53, servers: 484, featured: false, score: 4, reviews: 835, },
+        }
+        ctx.Data["recently_added_bots"] = []botModel{
+            {id: "123456789", name: "Vonodosh Bot", category: "Games", description: "Vonodosh. A bot that can fulfill your entertaining needs, with games to keep your guild busy.", website: "http://bit.ly/vonodosh", server: "PVHQxBd", authorize: "https://discordapp.com/oauth2/authorize?client_id=192332191240421377&scope=bot&permissions=536083519", avatar: "https://cdn.discordapp.com/app-icons/170242612425392128/8744452b039eaec1ba34c0a96c3bf428.jpg", online: true, ranking: 53, servers: 484, featured: false, score: 4, reviews: 835, },
+            {id: "123456789", name: "Vonodosh Bot", category: "Games", description: "Vonodosh. A bot that can fulfill your entertaining needs, with games to keep your guild busy.", website: "http://bit.ly/vonodosh", server: "PVHQxBd", authorize: "https://discordapp.com/oauth2/authorize?client_id=192332191240421377&scope=bot&permissions=536083519", avatar: "https://cdn.discordapp.com/app-icons/170242612425392128/8744452b039eaec1ba34c0a96c3bf428.jpg", online: true, ranking: 53, servers: 484, featured: false, score: 4, reviews: 835, },
+            {id: "123456789", name: "Vonodosh Bot", category: "Games", description: "Vonodosh. A bot that can fulfill your entertaining needs, with games to keep your guild busy.", website: "http://bit.ly/vonodosh", server: "PVHQxBd", authorize: "https://discordapp.com/oauth2/authorize?client_id=192332191240421377&scope=bot&permissions=536083519", avatar: "https://cdn.discordapp.com/app-icons/170242612425392128/8744452b039eaec1ba34c0a96c3bf428.jpg", online: true, ranking: 53, servers: 484, featured: false, score: 4, reviews: 835, },
+            {id: "123456789", name: "Vonodosh Bot", category: "Games", description: "Vonodosh. A bot that can fulfill your entertaining needs, with games to keep your guild busy.", website: "http://bit.ly/vonodosh", server: "PVHQxBd", authorize: "https://discordapp.com/oauth2/authorize?client_id=192332191240421377&scope=bot&permissions=536083519", avatar: "https://cdn.discordapp.com/app-icons/170242612425392128/8744452b039eaec1ba34c0a96c3bf428.jpg", online: true, ranking: 53, servers: 484, featured: false, score: 4, reviews: 835, },
+            {id: "123456789", name: "Vonodosh Bot", category: "Games", description: "Vonodosh. A bot that can fulfill your entertaining needs, with games to keep your guild busy.", website: "http://bit.ly/vonodosh", server: "PVHQxBd", authorize: "https://discordapp.com/oauth2/authorize?client_id=192332191240421377&scope=bot&permissions=536083519", avatar: "https://cdn.discordapp.com/app-icons/170242612425392128/8744452b039eaec1ba34c0a96c3bf428.jpg", online: true, ranking: 53, servers: 484, featured: false, score: 4, reviews: 835, },
+        }
         ctx.HTMLSet(200, "base", "index")
     })
     m.Get("/bots/:id", func(ctx *macaron.Context) {
+        ctx.Data["bot"] = botModel{
+            id: "123456789",
+            name: "Vonodosh Bot",
+            category: "Games",
+            description: "Vonodosh. A bot that can fulfill your entertaining needs, with games to keep your guild busy.",
+            website: "http://bit.ly/vonodosh",
+            server: "PVHQxBd",
+            authorize: "https://discordapp.com/oauth2/authorize?client_id=192332191240421377&scope=bot&permissions=536083519",
+            avatar: "https://cdn.discordapp.com/app-icons/170242612425392128/8744452b039eaec1ba34c0a96c3bf428.jpg",
+            online: true,
+            ranking: 53,
+            servers: 484,
+            featured: false,
+            score: 4,
+            reviews: 835,
+        }
+        ctx.Data["content"] = "<img src=\"http://i.imgur.com/lvfXcZm.png\" class=\"image\">"
         ctx.HTMLSet(200, "base", "bot")
+
     })
     err = http.ListenAndServe(conf.http.port, m)
     if err != nil {
--- src/main/structs.go
+++ src/main/structs.go
@@ -1,12 +1,36 @@
 package main
 
 type (
+	botModel struct {
+		id          string
+		name        string
+		description string
+		category    string
+		website     string
+		server      string
+		authorize   string
+		avatar      string
+		online      bool
+		ranking     int
+		servers     int
+		featured    bool
+		score       int
+		reviews     int
+	}
 	config struct {
 		http struct {
 			port string
 		}
 		templates struct {
 			directory string
+		}
+		oauth struct {
+			client_id         string
+			client_secret     string
+			scopes            []string
+			redirect_url      string
+			authorization_url string
+			token_url         string
 		}
 	}
 )
